5edc357af543dcb0099ee085e8955ca176841799
James Dao <jmsdao@67.78.81.158>
2021-11-05 07:29:47 -0500

    Add pilot EDA

--- data/get_data.R
+++ data/get_data.R
@@ -1,15 +1,9 @@
 ################################################################################
-### PACKAGES
+### LIBRARIES
 ################################################################################
 
-# Package names
-packages <- c("here", "tidyverse")
-
-# Install standard packages not yet installed
-installed_packages <- packages %in% rownames(installed.packages())
-if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
-}
+library(here)
+library(tidyverse)
 
 
 ################################################################################
@@ -28,21 +22,29 @@ if (!file.exists(file_path)) {
 # Use zless in terminal to inspect files
 # If the data is raw, we should see only integers with occasional zeros
 
+# Preprocess data and save as R variables
 if (!file.exists(here("data", "counts.Rdata"))) {
   raw_files <- list.files(dir_path)
   
   df <- read.table(gzfile(here(dir_path, raw_files[1])), sep = "\t", header = FALSE)
-  colnames(df) <- c("gene_id", str_split(raw_files[1], "_")[[1]][1])
+  colnames(df) <- c("gene_id", sub("_.*$", "", raw_files[1]))
   
   for (raw_file in raw_files[2:length(raw_files)]) {
     temp_df <- read.table(gzfile(here(dir_path, raw_file)), sep = "\t", header = FALSE)
-    colnames(temp_df) <- c("gene_id", str_split(raw_file, "_")[[1]][1])
+    colnames(temp_df) <- c("gene_id", sub("_.*$", "", raw_file))
     df <- df %>% full_join(temp_df)
   }
   
   counts <- df %>%
     column_to_rownames("gene_id") %>%
     as.matrix()
+  counts <- counts[startsWith(rownames(counts), "ENSG"), ]
   
-  save(counts, file = here("data", "counts.Rdata"))
+  # Metadata from https://www.ncbi.nlm.nih.gov/Traces/study/?acc=PRJNA394766&o=acc_s%3Aa
+  # Not sure how to get this programmatically
+  metadata <- read.csv(here("data", "SraRunTable.txt")) %>%
+    filter(Assay.Type == "RNA-Seq") %>%
+    column_to_rownames("Sample.Name")
+  
+  save(counts, metadata, file = here("data", "counts.Rdata"))
 }
